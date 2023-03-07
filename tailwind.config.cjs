8514b8dd3cc747eb3e6b8ba45bf06d40240f959f
angvzrz <ang_vzrz@live.com.mx>
2023-03-06 23:06:56 -0600

    Custom header background color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {colors: {
+      'header': '#001541',
+    }},
   },
   plugins: [],
 };
