fdfbb37f74b470b630520552ecc80ce43b09e8a5
angvzrz <ang_vzrz@live.com.mx>
2023-03-08 13:50:44 -0600

    Set base url in environment variable

---
+++ src/services/places-api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { IPlace } from '../types/types';
+
+export function getPlaces(queryPlace: string): Promise<IPlace[]> {
+  const baseUrl = import.meta.env.VITE_API_PLACES_BASE_URL;
+
+  return axios
+    .get<IPlace[]>(`${baseUrl}?q=${queryPlace}`)
+    .then((response) => {
+      const places: IPlace[] = response.data.map((place) => ({
+        id: place.id,
+        display: place.display,
+        state: place.state,
+        lat: place.lat,
+        long: place.long,
+      }));
+
+      return places.slice(0, 10);
+    })
+    .catch((error) => {
+      console.error(error);
+      return [];
+    });
+}
