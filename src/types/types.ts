9b8ff8e1e0151ac453a8f779dac2eea0c8788e7c
angvzrz <ang_vzrz@live.com.mx>
2023-03-09 03:48:38 -0600

    Declare interface for handling properties from 5 day forecast response

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,11 +7,19 @@ export interface IPlace {
 }
 
 export interface IPlaceWeather {
-  place: string;
+  place?: string;
   temp: number;
   main: string;
   description: string;
   humidity: number;
   wind_speed: number;
   icon: string;
+}
+
+export interface IWeatherCard extends Omit<IPlaceWeather, 'main'> {}
+
+export interface I5DayForecast {
+  main: { humidity: number; temp: number };
+  weather: { main: string; icon: string }[];
+  wind: { speed: number };
 }
