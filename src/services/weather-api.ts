24506447ed50b60e35fcf04dd755f47f792e494d
angvzrz <ang_vzrz@live.com.mx>
2023-03-08 21:39:00 -0600

    Create file for open weather api requests. Implement function for getting location weather data and return object with necesary fields

--- src/services/weather-api.ts
+++ src/services/weather-api.ts
@@ -1,6 +1,32 @@
+import axios from 'axios';
 import { IPlaceWeather } from '../types/types';
 
 export function getWeather(
   lat: number,
-  long: number
-): Promise<IPlaceWeather> | void {}
+  lon: number
+): Promise<IPlaceWeather | {}> {
+  const baseUrl: string = import.meta.env.VITE_API_WEATHER_BASE_URL;
+  const apiKey: string = import.meta.env.VITE_API_WEATHER_KEY;
+  const urlRequest = `${baseUrl}?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  return axios
+    .get(urlRequest)
+    .then((response) => {
+      const weatherData = response.data;
+      const mainWeather: IPlaceWeather = {
+        place: weatherData.name,
+        temp: weatherData.main.temp,
+        main: weatherData.weather[0].main,
+        description: weatherData.weather[0].description,
+        humidity: weatherData.main.humidity,
+        wind_speed: weatherData.wind.speed,
+        icon: weatherData.weather[0].icon,
+      };
+
+      return mainWeather;
+    })
+    .catch((error) => {
+      console.error(error);
+      return {};
+    });
+}
