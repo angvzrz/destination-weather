1e81d3e508d821f659cce581761ca1e9b4621a56
angvzrz <ang_vzrz@live.com.mx>
2023-03-08 20:10:53 -0600

    Header styling for handling headline space

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,8 @@ import { SearchBar } from './search-bar';
 
 export function Header() {
   return (
-    <header className="flex w-full justify-between p-3 bg-header">
-      <h1 className="w-1/2 text-start text-2xl text-slate-100 grow-0 ">
+    <header className="flex w-full justify-between bg-header p-3">
+      <h1 className="w-1/2 grow-0 text-start text-2xl text-slate-100 ">
         Check the weather in your destination
       </h1>
       <SearchBar />
